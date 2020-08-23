ce031500a8e3cd944636f340595e7b4eaf8410d2
Alessio <alessio.vertemati@gmail.com>
2020-08-23 18:47:20 +0200

    Update version number

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const EncodeCommand = require("./commands/encode");
 const PackCommand = require("./commands/pack");
 const FetchBinariesCommand = require("./commands/fetch-binaries");
 
-program.version("0.5.3").on("--help", function() {
+program.version("0.6.0").on("--help", function() {
   // add the what's new section to the --help output
   Log.comment();
   Log.comment("  What's new in", program.version());
